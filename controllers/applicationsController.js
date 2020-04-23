e5444e453d98ffc168728f43fecc56fa1dc18cdb
Sebarg91 <sebastian.gaboto@hotmail.com>
2020-04-23 17:32:41 -0300

    avances con rutas

--- controllers/applicationsController.js
+++ controllers/applicationsController.js
@@ -1,5 +1,12 @@
-let applicationsController = {
+let db = require('../database/models');
 
+let applicationsController = {
+    crear: function(req, res){
+        db.categorie.findAll()
+        .then(function(categorie){
+           return res.render('listadoApp', {categorie: categorie})
+        })
+    }
 }
 
 module.exports = applicationsController
